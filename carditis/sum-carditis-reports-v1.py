60cb00166eb205760bd6eb1cc018438f5b4a55a5
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-02-15 01:28:26 +0900

    fix: 複数の一覧で重複している案件が2重に追加されていたのを修正

--- carditis/sum-carditis-reports-v1.py
+++ carditis/sum-carditis-reports-v1.py
@@ -10,7 +10,20 @@ for file in jsonFileList:
 		carditis_reports += data
 
 sorted_reports = sorted(carditis_reports, key=lambda issue: issue['no'])
-json_string = json.dumps(sorted_reports, ensure_ascii=False, indent=2)
+
+# 心筋炎と心膜炎で重複している案件もあるため、noとvaccine_nameが同じものは
+# 同一案件として片方だけを保存するようにする。
+previous_no = 0
+previous_vaccine_name = ''
+reports_to_save = []
+for report in sorted_reports:
+	if previous_no == report['no'] and previous_vaccine_name == report['vaccine_name']:
+		continue
+	reports_to_save.append(report)
+	previous_no = report['no']
+	previous_vaccine_name = report['vaccine_name']
+
+json_string = json.dumps(reports_to_save, ensure_ascii=False, indent=2)
 
 output_path = os.path.join(output_dir, 'carditis-reports.json')
 with open( output_path, "w", encoding='utf-8') as f:
