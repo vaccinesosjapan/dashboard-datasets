685f97fe11da45d4347061dfda93634649a3cc79
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-04-16 01:31:01 +0900

    feat: add medical institution issues reported on 2024-04-12

--- libraries/exmedical.py
+++ libraries/exmedical.py
@@ -1,3 +1,5 @@
+import unicodedata
+
 def create_graph_by_causal_relationship(issues):
 	graph = dict()
 
@@ -39,22 +41,54 @@ def create_graph_severities_of_related(issues):
 	sorted_list = sorted(graph.items(), key=lambda x: x[1], reverse=True)
 	return dict(sorted_list)
 
-def create_keys(issues, name):
-	graph = dict()
-	for issue in issues:
-		v = issue[name]
-		if v == '':
-			v = '(空白)'
-		graph[v] = 1
-	return list(graph.keys())
+
+def cleansing_vaccine_name(vaccine_name):
+	'''
+	vaccine_nameの抽出文字列において、変なところに改行が入っているケースが
+	多々ある。規則性があるわけでもないので、対処療法的に特定パターンに対して
+	変換処理を実施する。
+	'''
+	vaccine_name = vaccine_name.replace('\n歳用', '歳用')
+	vaccine_name = vaccine_name.replace('歳\n用', '歳用')
+	vaccine_name = vaccine_name.replace('１\n価', '１価')
+	vaccine_name = vaccine_name.replace('２\n価', '２価')
+	vaccine_name = vaccine_name.replace('価\n不明', '価不明')
+	vaccine_name = vaccine_name.replace('\n起源株', '起源株')
+	vaccine_name = vaccine_name.replace('起\n源株', '起源株')
+	vaccine_name = vaccine_name.replace('起源\n株', '起源株')
+	vaccine_name = vaccine_name.replace('株\nBA.1', '株BA.1')
+	vaccine_name = vaccine_name.replace('BA.4-\n5', 'BA.4-5')
+	vaccine_name = vaccine_name.replace('11歳用\n（', '11歳用（')
+	vaccine_name = vaccine_name.replace('オミクロ\nン', 'オミクロン')
+	vaccine_name = vaccine_name.replace('オミ\nクロン', 'オミクロン')
+	vaccine_name = vaccine_name.replace('オミクロン株\nXBB', 'オミクロン株XBB')
+	vaccine_name = vaccine_name.replace('オ\nミクロン株', 'オミクロン株')
+
+	return unicodedata.normalize("NFKC", vaccine_name)
 
 
-def create_keys_from_array(issues, name):
-	graph = dict()
-	for issue in issues:
-		vArray = issue[name]
-		for v in vArray:
-			if v == '':
-				v = '(空白)'
-			graph[v] = 1
-	return list(graph.keys())
+def create_unique_list(original_list):
+	'''
+	DataFrameの場合 df['gender'].unique().tolist() のようにすればユニークな
+	リストが得られるはずだが、それで得た項目をさらに改行で分割しつつユニーク
+	なリストに再構成したい場合が多々あった。そのための処理。
+	'''
+	result_list = list()
+	for item in original_list:
+		for data in item.split('\n'):
+			result_list.append(data)
+	
+	return list(set(result_list))
+
+
+def create_unique_list_with_2d_list(original_2d_list):
+	'''
+	DataFrameから取り出した値がさらに配列の場合に使う想定。2次元配列を1次元に
+	分解・再構成しながらユニークな1次元のリストにする。
+	'''
+	result_list = list()
+	for item_list in original_2d_list:
+		for item in item_list:
+			result_list.append(item)
+	
+	return list(set(result_list))
