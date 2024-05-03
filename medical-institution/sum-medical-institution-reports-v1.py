32004f0556192d2a440f91616e79d4df5474a225
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-05-03 16:13:28 +0900

    fix: property name

--- medical-institution/sum-medical-institution-reports-v1.py
+++ medical-institution/sum-medical-institution-reports-v1.py
@@ -74,7 +74,7 @@ metadata = metadata_root['metadata']
 sum_causal_relationship = create_graph_by_causal_relationship(sorted_issues)
 sum_severities_of_related = create_graph_severities_of_related(sorted_issues)
 
-unknown_lotno_df = df[df['lot_no'].map(lambda x: str(x).__contains__('不明'))]
+invalid_lotno_df = df[df['lot_no'].map(lambda x: str(x).__contains__('不明') or not str(x))]
 valid_lotno_df = df[df['lot_no'].map(lambda x: not str(x).__contains__('不明'))]
 
 summary_data = {
@@ -85,7 +85,7 @@ summary_data = {
 		"sum_severities_of_related": sum_severities_of_related,
 		"lot_no_info": {
 			"top_ten_list": valid_lotno_df.groupby(['lot_no'])['no'].count().nlargest(10).to_dict(),
-			"unknown_count": unknown_lotno_df.shape[0]
+			"invalid_count": invalid_lotno_df.shape[0]
 		},
 	}
 }
--- practices/try-to-create-lotno-summary.py
+++ practices/try-to-create-lotno-summary.py
@@ -1,5 +1,5 @@
-# ロットNoに「不明」が含まれているものをunknown_dfに保持し、
-# 含まれていないもの（集計対象）をvalid_dfに保持する。
+# ロットNoに「不明」が含まれているものや空白のものをinvalid_lotno_dfに保持し、
+# それ以外のもの（集計対象）をvalid_lotno_dfに保持する。
 # valid_dfのトップ10を抽出する。
 
 import os,json
@@ -10,14 +10,19 @@ output_dir = '../_datasets'
 json_file_path = os.path.join(output_dir, 'medical-institution-reports.json')
 df = pd.read_json(json_file_path)
 
-unknown_df = df[df['lot_no'].map(lambda x: str(x).__contains__('不明'))]
+print(f'total count: {df.shape[0]}')
+
+invalid_lotno_df = df[df['lot_no'].map(lambda x: str(x).__contains__('不明') or not str(x))]
 #print(unknown_df)
-unknown_df_count = unknown_df['no'].shape[0]
-print(f'unknown lot_no count: {unknown_df_count}')
+invalid_count = invalid_lotno_df['no'].shape[0]
+print(f'invalid lot_no count: {invalid_count}')
+
+valid_lotno_df = df[df['lot_no'].map(lambda x: not str(x).__contains__('不明'))]
+valid_count = valid_lotno_df['no'].shape[0]
+print(f'valid lot_no count: {valid_count}')
 print()
 
-valid_df = df[df['lot_no'].map(lambda x: not str(x).__contains__('不明'))]
-top_ten = valid_df.groupby(['lot_no'])['no'].count().nlargest(10)
+top_ten = valid_lotno_df.groupby(['lot_no'])['no'].count().nlargest(10)
 print(top_ten)
 print()
 
