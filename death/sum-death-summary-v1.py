c2bed9fb437dfcdfd2d7072c60bb2a53e4a0717e
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-10-20 23:11:04 +0900

    fix: 製造販売業者別の集計データを作成

--- death/sum-death-summary-v1.py
+++ death/sum-death-summary-v1.py
@@ -1,31 +1,50 @@
 import glob, json, os
 import yaml
+import pandas as pd
 
-jsonFileList = glob.glob('summary-data/*.json')
 output_dir = '../_datasets'
 
-death_issues = []
-alpha_list = []
-beta_list = []
-gamma_list = []
-for file in jsonFileList:
-	with open(file, "r", encoding='utf-8') as f:
-		data = json.load(f)
-		death_issues.append(data)
-		alpha_list.append(data['evaluations']['alpha'])
-		beta_list.append(data['evaluations']['beta'])
-		gamma_list.append(data['evaluations']['gamma'])
 
-sorted_issues = sorted(death_issues, key=lambda issue: issue['vaccine_name'])
+def sum_by_manufacturer():
+    json_file_path = os.path.join('../_datasets', 'death-reports.json')
+    with open(json_file_path, encoding='utf-8') as f:
+        json_data = json.load(f)
+    
+    df = pd.json_normalize(json_data)
+    df = df[["manufacturer", "causal_relationship_by_expert"]]
+    df['death_count'] = 1
+    
+    merged_df = df.groupby(['manufacturer', 'causal_relationship_by_expert'], as_index=False).sum()
+    merged_df = merged_df[(merged_df['causal_relationship_by_expert'] == 'β') | (merged_df['causal_relationship_by_expert'] == 'γ')]
+    merged_df = merged_df.drop(columns=['causal_relationship_by_expert'])
+    
+    return merged_df.groupby('manufacturer', as_index=False).sum()
 
-alpha_sum = sum(alpha_list)
-beta_sum = sum(beta_list)
-gamma_sum = sum(gamma_list)
-total_sum = sum([alpha_sum, beta_sum, gamma_sum])
+def sum_evaluations():
+    death_issues = []
+    alpha_count = 0
+    beta_count = 0
+    gamma_count = 0
+
+    jsonFileList = glob.glob('summary-data/*.json')
+    for file in jsonFileList:
+        with open(file, "r", encoding='utf-8') as f:
+            data = json.load(f)
+            death_issues.append(data)
+            alpha_count += data['evaluations']['alpha']
+            beta_count += data['evaluations']['beta']
+            gamma_count += data['evaluations']['gamma']
+    
+    sorted_issues = sorted(death_issues, key=lambda issue: issue['vaccine_name'])  
+   
+    return (sorted_issues, alpha_count, beta_count, gamma_count)
+
+
+(sorted_issues, alpha_count, beta_count, gamma_count) = sum_evaluations()
+total_count = alpha_count + beta_count + gamma_count
 
 with open('summary-metadata.yaml', "r", encoding='utf-8') as file:
     metadata_root = yaml.safe_load(file)
-
 metadata = metadata_root['metadata']
 
 summary_data = {
@@ -33,12 +52,13 @@ summary_data = {
         "date": metadata['summary']['date'],
 		"source": metadata['summary']['source'],
         "sum_by_evaluation": {
-			"total": total_sum,
-			"alpha": alpha_sum,
-			"beta": beta_sum,
-			"gamma": gamma_sum
+			"total": total_count,
+			"alpha": alpha_count,
+			"beta": beta_count,
+			"gamma": gamma_count
 		},
-        "sum_by_vaccine_name": sorted_issues
+        "sum_by_vaccine_name": sorted_issues,
+        "sum_by_manufacturer": sum_by_manufacturer().to_dict(orient='records')
 	},
 	"death_issues": {
         "date": metadata['issues']['date'],		
